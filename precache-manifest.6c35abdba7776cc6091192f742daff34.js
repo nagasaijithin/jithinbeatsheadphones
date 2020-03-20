a8ee01c4bfa299940f7e25727bf14659b454da89
nagasaijithin <nagasaijithin1@gmail.com>
2020-03-20 23:53:55 +0530

    Updates

---
+++ precache-manifest.6c35abdba7776cc6091192f742daff34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebf516c253880ac3f4bbf1ca7eb26f3",
+    "url": "/jithinbeatsheadphones/index.html"
+  },
+  {
+    "revision": "eb817c7de75612497837",
+    "url": "/jithinbeatsheadphones/static/css/main.89af8791.chunk.css"
+  },
+  {
+    "revision": "3de32501fdfd414fb0c8",
+    "url": "/jithinbeatsheadphones/static/js/2.df9aa0b6.chunk.js"
+  },
+  {
+    "revision": "0bdb97d421f97884a5b69362f0009415",
+    "url": "/jithinbeatsheadphones/static/js/2.df9aa0b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb817c7de75612497837",
+    "url": "/jithinbeatsheadphones/static/js/main.124f5a03.chunk.js"
+  },
+  {
+    "revision": "399317e7faaa4fd33567",
+    "url": "/jithinbeatsheadphones/static/js/runtime-main.21463c90.js"
+  },
+  {
+    "revision": "697033ed0c271e585a28c93472e23e17",
+    "url": "/jithinbeatsheadphones/static/media/BeatsLogo.697033ed.png"
+  },
+  {
+    "revision": "2edc59edab127d5f1bfffd37e0633fe7",
+    "url": "/jithinbeatsheadphones/static/media/BeatsPillPlusimage002.2edc59ed.png"
+  },
+  {
+    "revision": "f8684a02614cfe7e6979298831759b43",
+    "url": "/jithinbeatsheadphones/static/media/PillPlusFeat.f8684a02.png"
+  },
+  {
+    "revision": "0a1c3103792e57873d532668ce421670",
+    "url": "/jithinbeatsheadphones/static/media/eimg.0a1c3103.jpg"
+  },
+  {
+    "revision": "bbfb40fcbe18df6c03650467706e6da9",
+    "url": "/jithinbeatsheadphones/static/media/pillplusblack01.bbfb40fc.png"
+  },
+  {
+    "revision": "1e6179ad8b9c6b3695be2829b7ff4f29",
+    "url": "/jithinbeatsheadphones/static/media/unnamed.1e6179ad.jpg"
+  }
+]);
